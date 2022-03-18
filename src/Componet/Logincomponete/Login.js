6328005b2fd2e4f42436013e98e7b2db8c56f65d
avanish <avanishsakhareliya@gmail.com>
2022-03-18 15:00:51 +0530

    master

--- src/Componet/Logincomponete/Login.js
+++ src/Componet/Logincomponete/Login.js
@@ -1,60 +1,69 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import "./Login.css"
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import toastr from "toastr"
 import 'toastr/build/toastr.css';
-function Login (props) {
+import { GoogleLogin } from "react-google-login"
 
-    const [obj,setobj]=useState({email:"",password:""})
-    const history=useHistory()
+function Login(props) {
 
-    const handaleChange=(e)=>{
-          setobj({...obj,[e.target.name]:e.target.value})
+    const [obj, setobj] = useState({ email: "", password: "" })
+    const history = useHistory()
+
+    const handaleChange = (e) => {
+        setobj({ ...obj, [e.target.name]: e.target.value })
     }
+    const responseGoogle=async(googleData)=> {
+        const result = await axios.post("http://localhost:3000/googlelogin",{ token: googleData.tokenObj }, { userObj: googleData.profileObj })
 
+        console.log("result",result)
+        console.log("googledata-->",googleData)
+        navigator("/")
+
+    }
     const onLogin = async (e) => {
         e.preventDefault()
-        const userlogin = await axios.post("http://localhost:3000/loginuser", obj).catch((err)=>{
- console.log(err.response.status)
- if(err.response.status===401){
-    toast.error('password not match', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        });
- }
-else{
-    toast.error('invalid Users', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        });
-}
+        const userlogin = await axios.post("http://localhost:3000/loginuser", obj).catch((err) => {
+            console.log(err.response.status)
+            if (err.response.status === 401) {
+                toast.error('password not match', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
+            else {
+                toast.error('invalid Users', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
         })
 
         if (userlogin.status === 200) {
-        toastr.success('successfull login',{timeOut: 5000})
-            
+            toastr.success('successfull login', { timeOut: 5000 })
+
             console.log("successfull signup", userlogin)
             localStorage.setItem("token", userlogin.data.token)
-            setobj({ username: "", email: "", password: "", cpassword: "" }); 
-              
-            console.log("props,",props)
+            setobj({ username: "", email: "", password: "", cpassword: "" });
+
+            console.log("props,", props)
             history.push("/livemarket");
-           
+
         }
-        else{
+        else {
             // toastr.success('successfull login')
             toast.error('invalid Users', {
                 position: "top-right",
@@ -64,12 +73,12 @@ else{
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-                });
+            });
         }
     }
     return (
-       <>
-        <ToastContainer
+        <>
+            <ToastContainer
                 position="top-right"
                 autoClose={5000}
                 hideProgressBar={false}
@@ -88,12 +97,12 @@ else{
                                 <div class="card-body p-4 p-sm-5">
                                     <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
                                     <form>
-                                    <div class="form-floating mb-3">
-                                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" onChange={(e)=>handaleChange(e)} value={obj.email}/>
+                                        <div class="form-floating mb-3">
+                                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" onChange={(e) => handaleChange(e)} value={obj.email} />
                                             <label for="floatingInput">Email address</label>
                                         </div>
                                         <div class="form-floating mb-3">
-                                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" onChange={(e)=>handaleChange(e)} value={obj.password}/>
+                                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" onChange={(e) => handaleChange(e)} value={obj.password} />
                                             <label for="floatingPassword">Password</label>
                                         </div>
 
@@ -104,28 +113,37 @@ else{
                                             </label>
                                         </div>
                                         <div class="d-grid">
-                                            <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit" onClick={(e)=>{onLogin(e)}}>Sign in</button>
+                                            <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit" onClick={(e) => { onLogin(e) }}>Sign in</button>
                                         </div>
                                         <hr class="my-4" />
-                                            <div class="d-grid mb-2">
-                                                <button class="btn btn-google btn-login text-uppercase fw-bold" type="submit">
-                                                    <i class="fab fa-google me-2"></i> Sign in with Google
-                                                 </button>
-                                            </div>
-                                            <div class="d-grid">
-                                                <button class="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
-                                                    <i class="fab fa-facebook-f me-2"></i> Sign in with Facebook
-                                                </button>
-                                            </div>
-                                  </form>
-                                       </div>
+                                        <div class="d-grid mb-2">
+
+                                        <GoogleLogin
+                                                clientId="728916457886-1j3jf5e4rrsh0vktr6eudicca4tjm7pd.apps.googleusercontent.com"
+                                                buttonText="Login with Google"
+                                                onSuccess={responseGoogle}
+                                                onFailure={responseGoogle}
+                                                cookiePolicy={'single_host_origin'}
+                                            />
+{/* 
+                                            <button class="btn btn-google btn-login text-uppercase fw-bold" type="submit">
+                                                <i class="fab fa-google me-2"></i> Sign in with Google
+                                            </button> */}
+                                        </div>
+                                        <div class="d-grid">
+                                            <button class="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
+                                                <i class="fab fa-facebook-f me-2"></i> Sign in with Facebook
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-               
-       </>
+            </div>
+
+        </>
     );
 }
 
--- src/Componet/Logincomponete/Signup.js
+++ src/Componet/Logincomponete/Signup.js
@@ -3,50 +3,59 @@ import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import toastr from "toastr"
+import { GoogleLogin } from "react-google-login"
 import 'toastr/build/toastr.css';
 function Signup(props) {
     const [obj, setobj] = useState({ username: "", email: "", password: "", cpassword: "" })
-
     // localStorage.setItem("token","")
-
     const handaleChange = (e) => {
         setobj({ ...obj, [e.target.name]: e.target.value })
     }
+
+  const responseGoogle=async(googleData)=> {
+        const result = await axios.post("http://localhost:3000/googlelogin",{ token: googleData.tokenObj }, { userObj: googleData.profileObj })
+
+        console.log("result",result)
+        console.log("googledata-->",googleData)
+        navigator("/")
+
+    }
+
     const onSignin = async (e) => {
         e.preventDefault()
-        const userlogin = await axios.post("http://localhost:3000/signuser", obj).catch((err)=>{
- console.log(err.response.status)
- if(err.response.status===401){
-    toast.error('password not match', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        });
- }
-else{
-    toast.error('invalid Users', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        });
-}
+        const userlogin = await axios.post("http://localhost:3000/signuser", obj).catch((err) => {
+            console.log(err.response.status)
+            if (err.response.status === 401) {
+                toast.error('password not match', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
+            else {
+                toast.error('invalid Users', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
         })
 
         if (userlogin.status === 200) {
-            toastr.success('successfull singnup',{timeOut: 5000})
+            toastr.success('successfull singnup', { timeOut: 5000 })
             console.log("successfull signup", userlogin)
             localStorage.setItem("token", userlogin.data.token)
             setobj({ username: "", email: "", password: "", cpassword: "" })
         }
-        else{
+        else {
             toast.error('invalid Users', {
                 position: "top-right",
                 autoClose: 5000,
@@ -55,7 +64,7 @@ else{
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-                });
+            });
         }
     }
 
@@ -99,14 +108,24 @@ else{
                                         </div>
                                         <hr class="my-4" />
                                         <div class="d-grid mb-2">
+
+                                            <GoogleLogin
+                                                clientId="728916457886-1j3jf5e4rrsh0vktr6eudicca4tjm7pd.apps.googleusercontent.com"
+                                                buttonText="Login with Google"
+                                                onSuccess={responseGoogle}
+                                                onFailure={responseGoogle}
+                                                cookiePolicy={'single_host_origin'}
+                                            />
+
+                                            {/*                                             
                                             <button class="btn btn-google btn-login text-uppercase fw-bold" type="submit">
                                                 <i class="fab fa-google me-2"></i> Sign in with Google
-                                                 </button>
+                                                 </button> */}
                                         </div>
                                         <div class="d-grid">
                                             <button class="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
                                                 <i class="fab fa-facebook-f me-2"></i> Sign in with Facebook
-                                                </button>
+                                            </button>
                                         </div>
                                     </form>
                                 </div>
